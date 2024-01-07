9a01be3de6a08432e5a9948ff2d4308812139d12
Askhat <Askhat.Sam@gmail.com>
2024-01-07 21:53:48 +0600

    initial commit

---
+++ src/main/java/com/example/maintenancelogbook/MaintenanceLogbookApplication.java
@@ -0,0 +1,13 @@
+package com.example.maintenancelogbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceLogbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintenanceLogbookApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/maintenancelogbook/entity/log.java
@@ -0,0 +1,11 @@
+package com.example.maintenancelogbook.entity;
+
+import java.util.Date;
+
+public class log {
+    private long id;
+    private Date date;
+    private String aircraft;
+    private String task;
+
+}
