bb95dd248896738458bddf7b91f0238c2827a08f
Askhat <Askhat.Sam@gmail.com>
2024-01-08 22:54:32 +0600

    feat: connected to local postgres server

--- src/main/java/com/example/maintenancelogbook/entity/Log.java
+++ src/main/java/com/example/maintenancelogbook/entity/Log.java
@@ -1,11 +1,19 @@
 package com.example.maintenancelogbook.entity;
 
-import java.util.Date;
+import jakarta.persistence.*;
 
+import java.util.Date;
+@Entity
+@Table(name="log")
 public class Log {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
     private long id;
-    private Date date;
+    @Column(name="date")
+    private String date;
+    @Column(name="aircraft")
     private String aircraft;
+    @Column(name="task")
     private String task;
 
 }
--- src/main/java/com/example/maintenancelogbook/repository/LogRepository.java
+++ src/main/java/com/example/maintenancelogbook/repository/LogRepository.java
@@ -2,6 +2,8 @@ package com.example.maintenancelogbook.repository;
 
 import com.example.maintenancelogbook.entity.Log;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LogRepository extends JpaRepository<Log, Long> {
 }
